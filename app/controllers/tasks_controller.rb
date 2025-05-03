12544105979b7334e9e9f440d5bc1a7050bda9fd
Philipp Schlesinger <info@philcomm.dev>
2025-05-03 14:24:27 +0200

    Add inline edit functionality for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,38 @@
 class TasksController < ApplicationController
-  before_action :find_project, only: [ :index ]
+  before_action :find_project, only: [ :index, :show, :edit, :update ]
+  before_action :find_task, only: [ :show, :edit, :update ]
 
   def index
     @tasks = @project.tasks
+  end
+
+  def show
+    render :show, locals: { project: @project, task: @task }
+  end
+
+  def edit
+    render :edit, locals: { project: @project, task: @task }
+  end
+
+  def update
+    if @task.update(update_params)
+      render :show, locals: { project: @project, task: @task }
+    else
+      render :edit, locals: { project: @project, task: @task }
+    end
   end
 
   private
 
   def find_project
     @project = Project.find(params[:project_id])
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def update_params
+    params.require(:task).permit(:name)
   end
 end
