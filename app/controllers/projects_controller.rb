a3ec0cdd9abd052ba9d34f28b2d7935ea3885c7a
Philipp Schlesinger <info@philcomm.dev>
2025-05-03 14:02:39 +0200

    Add turbo powered inline editing for project names

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,33 @@
 class ProjectsController < ApplicationController
+  before_action :find_project, only: [ :show, :edit, :update ]
+
   def index
     @projects = Project.all
+  end
+
+  def show
+    render :show, locals: { project: @project }
+  end
+
+  def edit
+    render :edit, locals: { project: @project }
+  end
+
+  def update
+    if @project.update(update_params)
+      render :show, locals: { project: @project }
+    else
+      render :edit, locals: { project: @project }
+    end
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+
+  def update_params
+    params.require(:project).permit(:name)
   end
 end
